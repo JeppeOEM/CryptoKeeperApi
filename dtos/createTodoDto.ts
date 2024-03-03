72985a440d60735c79b1e547eec9fa55e95dad7a
Jeppe <jepp026b@edu.zealand.dk>
2024-03-03 12:05:29 +0100

    first commit

---
+++ dtos/createTodoDto.ts
@@ -0,0 +1,10 @@
+
+// In TypeScript, DTO stands for "Data Transfer Object." A DTO is a design pattern used to transfer data between software application
+// subsystems or layers, typically between the client and server in a distributed system. 
+//The primary purpose of a DTO is to encapsulate and transport data in a format that is easy to work with, 
+//efficient to transfer, and matches the communication requirements between different parts of the system.
+
+export default interface CreateTodoDto {
+    title: string;
+  }
+  
